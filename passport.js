1d53375997709ddddd70b44cc633c313fea6b19d
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-12-09 17:09:56 +0400

    add: google auth

--- passport.js
+++ passport.js
@@ -1,8 +1,11 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const GITHUB_CLIENT_ID = "2eccc3bd3e3813c2e9be"
 const GITHUB_CLIENT_SECRET = "d5a9a4ae51c5e4676ee8c9a42e61a0b980b20e6a"
+const GOOGLE_CLIENT_ID = "597192839397-vacsrmg2uoc1g4kg7au04auidihiq4hf.apps.googleusercontent.com"
+const GOOGLE_CLIENT_SECRET = "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"
 
 
 passport.use(new GitHubStrategy({
@@ -17,7 +20,22 @@ passport.use(new GitHubStrategy({
   }
 ));
 
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "auth/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
 passport.serializeUser((user,done)=>{
     done(null,user)
 })
 
+passport.deserializeUser((user,done)=>{
+  done(null,user)
+})
--- routes/auth.js
+++ routes/auth.js
@@ -29,8 +29,18 @@ router.get("/github", passport.authenticate("github", {scope:[
     "profile"
 ]}));
 
-router.get("/auth/github/callback",
+router.get("/github/callback",
     passport.authenticate("github", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+}));
+
+router.get("/google", passport.authenticate("google", {scope:[
+    "profile"
+]}));
+
+router.get("/google/callback",
+    passport.authenticate("google", {
     successRedirect: CLIENT_URL,
     failureRedirect: "/login/failed",
 }));
